87acc039d11a803864fcf0cabf95072c0848372e
khai <nguyendinhkhai2111997@gmail.com>
2019-08-05 10:39:26 +0700

    add window.web3

--- react-connect-contract/src/web3Service.js
+++ react-connect-contract/src/web3Service.js
@@ -1,18 +1,22 @@
 import Web3 from 'web3';
-
-const rpcURL = "https://ropsten.infura.io/v3/cf658270146645bca881f8a7d4752099" 
+const infuraKey = "cf658270146645bca881f8a7d4752099"; 
+const rpcURL = `https://ropsten.infura.io/v3/${infuraKey}`; 
 let web3cur = new Promise((resolve, reject)=>{
     window.addEventListener('load', async () => {
     if (typeof window.ethereum !== 'undefined') {
         try {
             await window.ethereum.enable();
             let cur = new Web3(window.ethereum);
-            console.log("Metamask");
+            console.log("Metamask ethereum");
             resolve(cur);
         } catch (error) {
             alert('Please allow access for the app to work');
             reject(null);
         }
+    } else if (window.web3) {
+        let cur = new Web3(window.web3.currentProvider);
+        console.log("Metamask web3");
+        resolve(cur);
     } else {
         let cur = new Web3(new Web3.providers.HttpProvider(rpcURL));
         console.log("No Metamask")
